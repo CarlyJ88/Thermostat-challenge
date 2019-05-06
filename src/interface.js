4d597ff746b68e9762dd83afa27fcd37a999bf3b
Carly <carly.jenkinson65@googlemail.com>
2019-05-06 13:44:07 +0100

    Added weather API to the interface.

--- src/interface.js
+++ src/interface.js
@@ -6,32 +6,45 @@ $(document).ready(function () {
     $('#temperature').attr('class', thermostat.currentEnergyUsage());
   }
 
-  updateTemperature()
+  updateTemperature();
 
   $('#up').on('click', function () {
     thermostat.up();
     updateTemperature();
-  })
+  });
 
   $('#down').on('click', function () {
     thermostat.down();
     updateTemperature();
-  })
+  });
 
   $('#resetTemperature').on('click', function () {
     thermostat.resetTemperature();
     updateTemperature();
-  })
+  });
 
   $('#turnOnPSM').on('click', function () {
     thermostat.turnOnPSM();
     $('#powerSavingModeActive').text('On')
     updateTemperature();
-  })
+  });
 
   $('#turnOffPSM').on('click', function () {
     thermostat.turnOffPSM();
     $('#powerSavingModeActive').text('Off')
     updateTemperature();
-  })
-})
+  });
+
+  function getCurrentCityTemp() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=9fbc1dd7e40d738d70395161f25368db&units=metric', function (data) {
+      $('#current-temperature').text(data.main.temp)
+    });
+  }
+
+  getCurrentCityTemp();
+
+  $('#current-city').change(function () {
+    getCurrentCityTemp();
+  });
+});
